520d2976989fcbb5b5270d8ca7406c61af2a1469
keithsterling <keith@keithsterling.com>
2016-11-24 09:21:01 +0000

    Fixed some references to old files in tests

--- src/test/aiml_tests/test_normalize_aiml.py
+++ src/test/aiml_tests/test_normalize_aiml.py
@@ -11,7 +11,7 @@ class BasicTestClient(TestClient):
     def load_configuration(self, arguments):
         super(BasicTestClient, self).load_configuration(arguments)
         self.configuration.brain_configuration._aiml_files = BrainFileConfiguration("/Users/keithsterling/Documents/Development/Python/Projects/AIML/program-y/src/test/aiml_tests/test_files/normalize", ".aiml", False)
-        self.configuration.brain_configuration._normal = "/Users/keithsterling/Documents/Development/Python/Projects/AIML/program-y/bots/keiffx/config/normal.txt"
+        self.configuration.brain_configuration._normal = "/Users/keithsterling/Documents/Development/Python/Projects/AIML/program-y/bots/rosie/config/normal.txt"
 
 class NormalizeAIMLTests(unittest.TestCase):
 
