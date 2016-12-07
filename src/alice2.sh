4bbca6e4477e38b3969622b68eb7213d0a57c109
Marco Amato <linediconsine@users.noreply.github.com>
2016-12-07 12:25:04 -0500

    Update alice2.sh
    In this code you were using your personal computer directory, I made more generic so can work also in other computers
    
    Let me know any feedback on this
    
    Thanks

--- src/alice2.sh
+++ src/alice2.sh
@@ -4,5 +4,5 @@ clear
 
 export PYTHONPATH=.
 
-python3 programy/clients.py --config /Users/keithsterling/Documents/Development/Python/Projects/AIML/program-y/bots/alice2/config.yaml --cformat yaml --logging /Users/keithsterling/Documents/Development/Python/Projects/AIML/program-y/bots/alice2/logging.yaml --debug
+python3 programy/clients.py --config ../bots/alice2/config.yaml --cformat yaml --logging ../bots/alice2/logging.yaml --bot_root ../bots/alice2/ --debug
 
