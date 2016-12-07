38ef5814f676f455b4db0de9efb0cc602703652d
Marco Amato <linediconsine@users.noreply.github.com>
2016-12-07 12:28:07 -0500

    Update rosie.sh
    Same as alice.sh
    
    Create a more generic directory matching

--- src/rosie.sh
+++ src/rosie.sh
@@ -4,5 +4,5 @@ clear
 
 export PYTHONPATH=.
 
-python3 programy/clients.py --config /Users/keithsterling/Documents/Development/Python/Projects/AIML/program-y/bots/rosie/config.yaml --cformat yaml --logging /Users/keithsterling/Documents/Development/Python/Projects/AIML/program-y/bots/rosie/logging.yaml --debug
+python3 programy/clients.py --config ../bots/rosie/config.yaml --cformat yaml --logging ../bots/rosie/logging.yaml --debug
 
