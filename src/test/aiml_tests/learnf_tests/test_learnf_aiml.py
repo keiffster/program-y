0ec4e5e528d41c524c1730fdad5a5cc4e427b0d8
keithsterling <keith@keithsterling.com>
2017-03-27 08:30:28 +0100

    Added better unit testing of learnf, now actually checks the contents of the file, rather than just check file exists

--- src/test/aiml_tests/learnf_tests/test_learnf_aiml.py
+++ src/test/aiml_tests/learnf_tests/test_learnf_aiml.py
@@ -1,7 +1,10 @@
 import unittest
 import os
 import os.path
+import xml.etree.ElementTree as ET
+
 from test.aiml_tests.client import TestClient
+
 from programy.config.brain import BrainFileConfiguration
 
 class LearnfTestClient(TestClient):
@@ -28,7 +31,7 @@ class LearnfAIMLTests(unittest.TestCase):
         response = LearnfAIMLTests.test_client.bot.ask_question("test", "MY NAME IS FRED")
         self.assertIsNotNone(response)
         self.assertEqual(response, "OK, I will remember your name is FRED")
-        self.check_file_contents()
+        self.check_file_contents("WHAT IS MY NAME", "*", "*", "YOUR NAME IS FRED")
 
         response = LearnfAIMLTests.test_client.bot.ask_question("test", "WHAT IS MY NAME")
         self.assertIsNotNone(response)
@@ -40,11 +43,33 @@ class LearnfAIMLTests(unittest.TestCase):
         response = LearnfAIMLTests.test_client.bot.ask_question("test", "JOHN PLAYED CRICKET")
         self.assertIsNotNone(response)
         self.assertEqual(response, "Ok. I will remember this")
-        self.check_file_contents()
+        self.check_file_contents("WHAT DID JOHN PLAY", "*", "*", "JOHN PLAYED CRICKET")
 
         response = LearnfAIMLTests.test_client.bot.ask_question("test", "WHAT DID JOHN PLAY")
         self.assertIsNotNone(response)
         self.assertEqual(response, "JOHN PLAYED CRICKET")
 
-    def check_file_contents(self):
+    def check_file_contents(self, pattern, topic, that, template):
         self.assertTrue(os.path.exists(self.learnf_path))
+
+        tree = ET.parse(self.learnf_path)
+        aiml = tree.getroot()
+
+        categories = aiml.findall('category')
+        self.assertEquals(1, len(categories))
+
+        patterns = categories[0].findall('pattern')
+        self.assertEquals(1, len(patterns))
+        self.assertEquals(patterns[0].text, pattern)
+
+        topics = categories[0].findall('topic')
+        self.assertEquals(1, len(topics))
+        self.assertEquals(topics[0].text, topic)
+
+        thats = categories[0].findall('that')
+        self.assertEquals(1, len(thats))
+        self.assertEquals(thats[0].text, that)
+
+        templates = categories[0].findall('template')
+        self.assertEquals(1, len(templates))
+        self.assertEquals(templates[0].text, template)
