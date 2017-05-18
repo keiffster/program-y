45603df3312afab7d88baf0fc19340a49362e4d8
keithsterling <keith@keithsterling.com>
2017-05-18 09:00:03 +0100

    Initial commit of adding grammar tests to y-bot, more to follow

---
+++ bots/y-bot/y-bot_tests.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+
+clear
+
+export PYTHONPATH=../../src:../../libs/MetOffer-1.3.2:.
+
+python3 ../../src/programy/clients/test_runner.py --test_dir ./tests --config ./config.yaml --cformat yaml --logging ./logging.yaml --debug
+
