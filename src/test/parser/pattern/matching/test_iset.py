bc5404d243fb060bd27d47d1c022dcc6a6e1aad4
keithsterling <keith@keithsterling.com>
2017-05-25 11:50:54 +0100

    Fixed an issue with iset, while upgrading a number of unit and integration tests

--- src/test/parser/pattern/matching/test_iset.py
+++ src/test/parser/pattern/matching/test_iset.py
@@ -16,3 +16,18 @@ class PatternMatcherISetTests(PatternMatcherBaseClass):
         self.assertIsNotNone(context)
         self.assertIsNotNone(context.template_node())
         self.assertEqual("1", context.template_node().template.word)
+
+    def test_multiple_iset_match(self):
+
+        self.add_pattern_to_graph(pattern="I LIKE TO <iset>PARTY, DRINK, SLEEP</iset> DURING THE DAY", topic="*", that="*", template="1")
+        self.add_pattern_to_graph(pattern="I LIKE TO <iset>PARTY, DRINK, SLEEP</iset> DURING THE NIGHT", topic="*", that="*", template="2")
+
+        context = self.match_sentence("I LIKE TO PARTY DURING THE DAY", topic="*", that="*")
+        self.assertIsNotNone(context)
+        self.assertIsNotNone(context.template_node())
+        self.assertEqual("1", context.template_node().template.word)
+
+        context = self.match_sentence("I LIKE TO PARTY DURING THE NIGHT", topic="*", that="*")
+        self.assertIsNotNone(context)
+        self.assertIsNotNone(context.template_node())
+        self.assertEqual("2", context.template_node().template.word)
