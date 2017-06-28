b2c10df1f1ee657d6fadbb18b498e75892674807
keithsterling <keith@keithsterling.com>
2017-06-28 17:20:45 +0100

    Added more unit tests

--- src/test/parser/template/node_tests/test_date.py
+++ src/test/parser/template/node_tests/test_date.py
@@ -3,6 +3,7 @@ import xml.etree.ElementTree as ET
 from programy.parser.template.nodes.base import TemplateNode
 from programy.parser.template.nodes.date import TemplateDateNode
 from programy.parser.template.nodes.word import TemplateWordNode
+from programy.parser.exceptions import ParserException
 
 from test.parser.template.base import TemplateTestsBaseClass
 
@@ -24,7 +25,20 @@ class TemplateDateNodeTests(TemplateTestsBaseClass):
         self.assertEqual(len(root.children), 1)
         self.assertRegex(root.resolve(self.bot, self.clientid), TemplateDateNodeTests.DEFAULT_DATETIME_REGEX)
 
-    def test_node_customformat(self):
+    def test_node_customformat_constructor(self):
+        root = TemplateNode()
+        self.assertIsNotNone(root)
+        self.assertIsNotNone(root.children)
+        self.assertEqual(len(root.children), 0)
+
+        node = TemplateDateNode(date_format="%c")
+        self.assertIsNotNone(node)
+
+        root.append(node)
+        self.assertEqual(len(root.children), 1)
+        self.assertRegex(root.resolve(self.bot, self.clientid), TemplateDateNodeTests.DEFAULT_DATETIME_REGEX)
+
+    def test_node_customformat_parameter(self):
         root = TemplateNode()
         self.assertIsNotNone(root)
         self.assertIsNotNone(root.children)
@@ -32,10 +46,37 @@ class TemplateDateNodeTests(TemplateTestsBaseClass):
 
         node = TemplateDateNode()
         self.assertIsNotNone(node)
+        node.format = "%c"
+        root.append(node)
+
+        self.assertEqual(len(root.children), 1)
+        self.assertEquals("%c", node.format)
+        self.assertRegex(root.resolve(self.bot, self.clientid), TemplateDateNodeTests.DEFAULT_DATETIME_REGEX)
+
+    def test_node_customformat_set_attrib(self):
+        root = TemplateNode()
+        self.assertIsNotNone(root)
+        self.assertIsNotNone(root.children)
+        self.assertEqual(len(root.children), 0)
+
+        node = TemplateDateNode()
+        self.assertIsNotNone(node)
+        node.set_attrib("format", "%c")
 
         root.append(node)
         self.assertEqual(len(root.children), 1)
         self.assertRegex(root.resolve(self.bot, self.clientid), TemplateDateNodeTests.DEFAULT_DATETIME_REGEX)
+
+    def test_node_customformat_set_attrib_invalid(self):
+        root = TemplateNode()
+        self.assertIsNotNone(root)
+        self.assertIsNotNone(root.children)
+        self.assertEqual(len(root.children), 0)
+
+        node = TemplateDateNode()
+        self.assertIsNotNone(node)
+        with self.assertRaises(ParserException):
+            node.set_attrib("unknown", "%c")
 
     def test_to_xml_defaults(self):
         root = TemplateNode()
---
+++ src/test/parser/template/node_tests/test_extension.py
@@ -0,0 +1,70 @@
+import xml.etree.ElementTree as ET
+
+from programy.parser.template.nodes.base import TemplateNode
+from programy.parser.template.nodes.extension import TemplateExtensionNode
+from programy.parser.template.nodes.word import TemplateWordNode
+
+from test.parser.template.base import TemplateTestsBaseClass
+
+class MockExtension(object):
+    def execute(self, bot, clientid, data):
+        if data is None or data is "":
+            return "VALID"
+        else:
+            return data
+
+class TemplateExtensionNodeTests(TemplateTestsBaseClass):
+
+    def test_node_no_data(self):
+        root = TemplateNode()
+        self.assertIsNotNone(root)
+        self.assertIsNotNone(root.children)
+        self.assertEqual(len(root.children), 0)
+
+        node = TemplateExtensionNode()
+        self.assertIsNotNone(node)
+        self.assertIsNone(node.path)
+
+        node.path = "test.parser.template.node_tests.test_extension.MockExtension"
+        self.assertEqual("test.parser.template.node_tests.test_extension.MockExtension", node.path)
+
+        root.append(node)
+        self.assertEqual(len(root.children), 1)
+
+        self.assertEqual(root.resolve(self.bot, self.clientid), "VALID")
+
+    def test_node_with_data(self):
+        root = TemplateNode()
+        self.assertIsNotNone(root)
+        self.assertIsNotNone(root.children)
+        self.assertEqual(len(root.children), 0)
+
+        node = TemplateExtensionNode()
+        self.assertIsNotNone(node)
+        self.assertIsNone(node.path)
+
+        node.append(TemplateWordNode("Test"))
+
+        node.path = "test.parser.template.node_tests.test_extension.MockExtension"
+        self.assertEqual("test.parser.template.node_tests.test_extension.MockExtension", node.path)
+
+        root.append(node)
+        self.assertEqual(len(root.children), 1)
+
+        self.assertEqual(root.resolve(self.bot, self.clientid), "Test")
+
+    def test_to_xml(self):
+        root = TemplateNode()
+        self.assertIsNotNone(root)
+        self.assertIsNotNone(root.children)
+        self.assertEqual(len(root.children), 0)
+
+        node = TemplateExtensionNode()
+        node.path = "test.parser.template.node_tests.test_extension.MockExtension"
+        node.append(TemplateWordNode("Test"))
+        root.append(node)
+
+        xml = root.xml_tree(self.bot, self.clientid)
+        self.assertIsNotNone(xml)
+        xml_str = ET.tostring(xml, "utf-8").decode("utf-8")
+        self.assertEqual('<template><extension path="test.parser.template.node_tests.test_extension.MockExtension">Test</extension></template>', xml_str)
