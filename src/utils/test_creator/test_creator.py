d59d3f5bfc7255f4ae6e524d952ad1b8ab7e1582
keithsterling <keith@keithsterling.com>
2017-07-12 17:48:19 +0100

    Added initial utils directory at top level and added test_creator utility

---
+++ src/utils/test_creator/test_creator.py
@@ -0,0 +1,45 @@
+import xml.etree.ElementTree as ET
+import sys
+
+if __name__ == '__main__':
+
+    print(sys.argv)
+
+    input = sys.argv[1]
+    output = sys.argv[2]
+
+    with open(output, "w+") as output_file:
+        try:
+            tree = ET.parse(input)
+            aiml = tree.getroot()
+            categories = aiml.findall('category')
+            for category in categories:
+                pattern = category.find("pattern")
+                sets = pattern.findall('set')
+                bots = pattern.findall('bot')
+                if len(sets) > 0:
+                    pattern_text = ""
+                    if pattern.text is not None:
+                        pattern_text += pattern.text
+                    pattern_text += " SET[%s] "%sets[0].text
+                    if pattern.tail is not None:
+                        pattern_text += pattern.tail.strip()
+                elif len(bots) > 0:
+                    pattern_text = ""
+                    if pattern.text is not None:
+                        pattern_text += pattern.text
+                    pattern_text += " BOT[%s] "%bots[0].text
+                    if pattern.tail is not None:
+                        pattern_text += pattern.tail.strip()
+                else:
+                    pattern_text = pattern.text
+                template = category.find('template')
+                print('"%s", "%s"'%(pattern_text, template.text))
+
+            topics = aiml.findall('topic')
+            if len(topics) > 0:
+                print("I dont handle topics yet!")
+
+        except Exception as e:
+            print(e)
+
