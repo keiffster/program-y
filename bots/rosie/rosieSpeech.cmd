20bfff26151bc5e12cd314fe8e19786e766feaed
Daryl Fung <darylfung96@gmail.com>
2017-08-24 13:25:16 -0500

    Speech support for rosie and professor

---
+++ bots/rosie/rosieSpeech.cmd
@@ -0,0 +1,8 @@
+@echo off
+
+CLS
+
+SET PYTHONPATH=..\..\src;..\..\libs\MetOffer-1.3.2;.
+
+python ..\..\src\programy\clients\console.py --config .\config.windows.yaml --cformat yaml --bot_type 1 --logging .\logging.windows.yaml
+
