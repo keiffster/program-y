5221495e85601d3eb1fd83dc8b8c5c66b44f17ba
keithsterling <keith@keithsterling.com>
2017-09-26 08:05:11 +0100

    Added a unit test to check topic matching, and new .cmd files for y-bot on windows

---
+++ bots/y-bot/y-bot-xmpp.cmd
@@ -0,0 +1,8 @@
+#! /bin/sh
+
+clear
+
+export PYTHONPATH=../../src:../../libs/MetOffer-1.3.2:.
+
+python3 ../../src/programy/clients/xmpp.py --config ./config.yaml --cformat yaml --logging ./logging.yaml
+
---
+++ test/programytest/aiml_tests/topic_tests/test_gimisa_aiml.py
@@ -0,0 +1,47 @@
+import unittest
+import os
+from programytest.aiml_tests.client import TestClient
+from programy.config.sections.brain.file import BrainFileConfiguration
+
+class TopicTestClient(TestClient):
+
+    def __init__(self):
+        TestClient.__init__(self)
+
+    def load_configuration(self, arguments):
+        super(TopicTestClient, self).load_configuration(arguments)
+        self.configuration.brain_configuration.files.aiml_files._files = [os.path.dirname(__file__)]
+
+class TopicAIMLTests(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        TopicAIMLTests.test_client = TopicTestClient()
+
+    def test_topic_single_topic_word(self):
+
+        response = TopicAIMLTests.test_client.bot.ask_question("test", "HI THERE")
+        self.assertIsNotNone(response)
+        self.assertEqual(response, 'OUTSIDE OF TEST1TOPIC')
+
+        response = TopicAIMLTests.test_client.bot.ask_question("test", "HELLO")
+        self.assertIsNotNone(response)
+        self.assertEqual(response, 'INSIDE OF TEST1TOPIC')
+
+    def test_topic_multiple_topic_words(self):
+        response = TopicAIMLTests.test_client.bot.ask_question("test", "SEEYA")
+        self.assertIsNotNone(response)
+        self.assertEqual(response, 'OUTSIDE OF TEST2 TOPIC')
+
+        response = TopicAIMLTests.test_client.bot.ask_question("test", "GOODBYE")
+        self.assertIsNotNone(response)
+        self.assertEqual(response, 'INSIDE OF TEST2 TOPIC')
+
+    def test_topic_wildcard_topic_words(self):
+        response = TopicAIMLTests.test_client.bot.ask_question("test", "WILDCARD1")
+        self.assertIsNotNone(response)
+        self.assertEqual(response, 'WILDCARD1 TEST OK')
+
+        response = TopicAIMLTests.test_client.bot.ask_question("test", "WILDCARD2")
+        self.assertIsNotNone(response)
+        self.assertEqual(response, 'WILDCARD2 TEST OK')
