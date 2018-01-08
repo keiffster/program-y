d3858e87741bbaabecbdc6c9c12b78f7a7923472
keithsterling <keith@keithsterling.com>
2018-01-08 17:27:03 +0000

    Docket files from bot dev team and contributors on how to create Ubuntu and Centos docket images

--- bots/y-bot/servusai.sh
+++ bots/y-bot/servusai.sh
@@ -7,3 +7,4 @@ export PYTHONPATH=/opt/program-y/src:.
 cd /opt/program-y
 
 python3 /opt/program-y/src/programy/clients/webchat/chatsrv.py --config /opt/program-y/bots/y-bot/config.yaml --cformat yaml --logging /opt/program-y/bots/y-bot/logging.yaml
+:wq
