549b236a7da0d473283d2b845bfb2b79647f90d1
keithsterling <keith@keithsterling.com>
2018-01-19 16:10:57 +0000

    Initial setup and config for PyPi submission

--- src/setup.py
+++ src/setup.py
@@ -1,8 +1,9 @@
 from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
   name = 'programy',
-  packages = ['programy'],
+  packages=find_packages(),
   version = '1.8.0',
   description = 'AIML 2.0 Framework and Platform',
   author = 'Keith Sterling',
@@ -10,5 +11,37 @@ setup(
   url = 'https://github.com/keiffster/program-y.git',
   download_url = 'https://github.com/keiffster/program-y/archive/1.8.0.tar.gz',
   keywords = ['aiml', 'chatbot', 'virtual assistant', 'ai'],
-  classifiers = [],
+  classifiers = [
+      # How mature is this project? Common values are
+      #   3 - Alpha
+      #   4 - Beta
+      #   5 - Production/Stable
+      'Development Status :: 5 - Production/Stable',
+
+      # Indicate who your project is intended for
+      'Intended Audience :: Developers',
+
+      # Pick your license as you wish (should match "license" above)
+      'License :: OSI Approved :: MIT License',
+
+      # Specify the Python versions you support here. In particular, ensure
+      # that you indicate whether you support Python 2, Python 3 or both.
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: 3.2',
+      'Programming Language :: Python :: 3.3',
+      'Programming Language :: Python :: 3.4',
+      'Programming Language :: Python :: 3.5',
+      'Programming Language :: Python :: 3.6',
+  ],
+  install_requires=['requests',
+                    'flask',
+                    'python-dateutil',
+                    'beautifulsoup4',
+                    'lxml',
+                    'wikipedia',
+                    'pyyaml',
+                    'tweepy',
+                    'sleekxmpp',
+                    'metoffer']
+
 )
