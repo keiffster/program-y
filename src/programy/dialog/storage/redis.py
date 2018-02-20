48a5a8110a5a4dfdff23af8052faa22b237fd51e
Diwank Tomer <diwank.singh@gmail.com>
2018-02-20 15:59:17 -0800

    Redis performance improvements and finishing unimplemented fns. Closes #135
    Signed-off-by: Diwank Tomer <diwank.singh@gmail.com>

--- src/programy/dialog/storage/redis.py
+++ src/programy/dialog/storage/redis.py
@@ -25,26 +25,119 @@ class ConversationRedisStorage(ConversationStorage):
 
     def __init__(self, config):
         ConversationStorage.__init__(self, config)
-        self._redis = redis.StrictRedis(host=config.host, port=config.port, db=0)
+        self._redis = redis.StrictRedis(
+            host=config.host,
+            port=config.port,
+            password=config.password,
+            db=0)
+
+        self._prefix = config.prefix
+
+        self._sessions_set_key = "{prefix}:sessions".format( prefix=self._prefix )
+
+        # Make sure the client is able to connect to the redis instance
+        assert self._redis.echo("test"), "Failed to connect to redis instance"
 
     def empty(self):
-        pass
+
+        if logging.getLogger().isEnabledFor(logging.DEBUG):
+            logging.debug("Deleting Conversation redis data")
+
+        redis = self._redis
+        s_key = self._sessions_set_key
+
+        try:
+            redis.delete( s_key )
+
+        except Exception as e:
+            if logging.getLogger().isEnabledFor(logging.ERROR):
+                logging.error("Failed deleting conversation redis data")
+                logging.exception(e)
 
     def save_conversation(self, conversation, clientid):
+
         if logging.getLogger().isEnabledFor(logging.DEBUG):
             logging.debug("Saving conversation to Redis for %s"%clientid)
+
         print("Writing to redis for %s"%clientid)
-        self._redis.hmset(clientid, conversation._properties)
+
+        redis = self._redis
+        pipeline = redis.pipeline()
+
+        clientid = str(clientid)
+        s_key = self._sessions_set_key
+        h_key = "{prefix}:{clientid}:props".format(
+            prefix = self._prefix,
+            clientid = clientid )
+
+        props = conversation._properties
+
+        try:
+            # Add clientid to sessions set
+            pipeline.sadd( s_key, clientid )
+
+            # Save properties
+            pipeline.hmset( h_key, props )
+            pipeline.execute()
+
+        except Exception as e:
+            if logging.getLogger().isEnabledFor(logging.ERROR):
+                logging.error("Failed to save conversation to redis for clientid [%s]"%clientid)
+                logging.exception(e)
 
     def load_conversation(self, conversation, clientid, restore_last_topic=False):
+
         if logging.getLogger().isEnabledFor(logging.DEBUG):
             logging.debug("Loading Conversation from file for %s"%clientid)
 
         print("Reading from redis for %s"%clientid)
-        result = self._redis.hgetall(clientid)
-        if result is not None:
-            conversation._properties = result
+
+        redis = self._redis
+
+        clientid = str(clientid)
+        s_key = self._sessions_set_key
+        h_key = "{prefix}:{clientid}:props".format(
+            prefix = self._prefix,
+            clientid = clientid )
+
+        conversation._properties = getattr(conversation, "_properties", {})
+
+        try:
+            # Check if clientid in sessions set
+            convo_exists = redis.sismember( s_key, clientid )
+
+            if not convo_exists:
+                return
+
+            # Fetch properties
+            props = redis.hgetall( h_key )
+            last_topic = props.pop("topic", None)
+
+            # Update conversation
+            conversation._properties.update(props)
+
+            # Load last topic if required
+            if restore_last_topic and last_topic:
+                conversation._properties["topic"] = last_topic
+
+        except Exception as e:
+            if logging.getLogger().isEnabledFor(logging.ERROR):
+                logging.error("Failed to load conversation from redis for clientid [%s]"%clientid)
+                logging.exception(e)
 
     def remove_conversation(self, clientid):
-        pass
 
+        if logging.getLogger().isEnabledFor(logging.DEBUG):
+            logging.debug("Deleting Conversation redis data")
+
+        redis = self._redis
+        s_key = self._sessions_set_key
+        clientid = str(clientid)
+
+        try:
+            redis.srem( s_key, clientid )
+
+        except Exception as e:
+            if logging.getLogger().isEnabledFor(logging.ERROR):
+                logging.error("Failed deleting conversation redis data for clientid [%s]"%clientid)
+                logging.exception(e)
