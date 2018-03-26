fab8892cb48916e7f54a4107eadfd5cecd01b106
keithsterling <keith@keithsterling.com>
2018-03-26 14:53:31 +0100

    Updated servusai.com config

---
+++ bots/servusai/install-services.sh
@@ -0,0 +1,43 @@
+#! /bin/sh
+
+cd rest
+install-services.sh
+
+cd ../web
+install-services.sh
+
+#cd ../facebook
+#./install.sh
+
+#cd ../kik
+#./install.sh
+
+#cd ../line
+#./install.sh
+
+#cd ../slack
+#./install.sh
+
+#cd ../socket
+#./install.sh
+
+#cd ../telegram
+#./install.sh
+
+#cd ../twilio
+#./install.sh
+
+#cd ../tritter
+#./install.sh
+
+# cd ../viber
+# ./install.sh
+
+#cd ../xmpp
+#./install.sh
+
+journalctl -xe
+
+
+
+
