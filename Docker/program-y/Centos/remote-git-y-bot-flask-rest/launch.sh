336dcdaac941bec4bf15587914e5504573994f4e
keithsterling <keith@keithsterling.com>
2018-04-23 11:53:45 +0100

    Moved docker files to own repo docker-y

--- Docker/program-y/Centos/remote-git-y-bot-flask-rest/launch.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Run python sever as program-y user (not root)
-`su -c 'cd /home/program-y/program-y/bots/y-bot/ && /home/program-y/program-y/bots/y-bot/y-bot-flask-rest.sh' - program-y`
