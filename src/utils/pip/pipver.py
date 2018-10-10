8ac5ccbe4c10a949add3c453342dfe1ebf9367de
keithsterling <keith@keithsterling.com>
2018-10-10 17:01:17 +0100

    Updated requirements.txt with latest versions of all libraries

---
+++ src/utils/pip/pipver.py
@@ -0,0 +1,24 @@
+import json
+from urllib.request import urlopen, Request
+
+def get_current_versions(req_file):
+
+    with open(req_file, "r") as file:
+        for line in file:
+            line = line.strip()
+            if line:
+                if line.startswith('#') is False:
+                    name_ver = line.split('==')
+                    try:
+                        url = "https://pypi.python.org/pypi/%s/json" % (name_ver[0])
+                        data = json.load(urlopen(Request(url)))
+                        versions = list(data["releases"].keys())
+                        versions = sorted(versions)
+                        latest = versions[len(versions) - 1]
+                        print("Package: %s, current: %s, latest: %s" % (name_ver[0], name_ver[1], latest))
+                    except Exception as e:
+                        print(e)
+
+if __name__ == '__main__':
+
+    get_current_versions("../../../requirements.txt")
