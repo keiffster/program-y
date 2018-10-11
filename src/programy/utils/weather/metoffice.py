b3b7bb43e0668f6ab563f1c6488eb9ed97d93cf4
keithsterling <keith@keithsterling.com>
2018-10-11 09:41:10 +0100

    Rolled back changes to met office, new version too different for basic upgrade

--- src/programy/utils/weather/metoffice.py
+++ src/programy/utils/weather/metoffice.py
@@ -16,7 +16,6 @@ TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR TH
 """
 
 import datetime
-import json
 from programy.utils.logging.ylogger import YLogger
 
 import metoffer
